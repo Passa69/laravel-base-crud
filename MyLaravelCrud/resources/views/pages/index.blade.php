9ec552f4f1fcae4b219b8214d9cfd46090ec2e4e
tommaso <passarotto13@gmail.com>
2022-01-28 17:04:48 +0100

    aggiunto link in index per home

--- MyLaravelCrud/resources/views/pages/index.blade.php
+++ MyLaravelCrud/resources/views/pages/index.blade.php
@@ -6,5 +6,7 @@
     <h4>{{ $comic -> title }}</h4>
     <span>{{ $comic -> author }}</span><br>
     <span>Release: {{ $comic -> release_date }} - {{ $comic -> pages }} pagine</span>
+    <br><br>
+    <span><a href="{{ route('home')}}">Torna all'elenco</a></span>
     <hr>
 @endsection
